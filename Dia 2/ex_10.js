975bfa736cebb6910ace221d78ed4e8862a9e4ee
Cleber-0101 <cleberbatista99@hotmail.com>
2025-07-28 08:23:02 -0300

    Criando objetos em Java Script

--- Dia 2/ex_10.js
+++ Dia 2/ex_10.js
@@ -3,3 +3,5 @@ let contador = 0
 for(let i = 1; i <= 5 ; i++){
     console.log(i)
 }
+
+
---
+++ Dia 3/objetos.js
@@ -0,0 +1,14 @@
+// obejtivos criar 20  objetos em Java script como exercios 
+
+//caneta 
+
+let caneta = {
+    name: "Bic",
+    cor: "blue",
+    isFunciona: true,
+    idade: 70 + "Anos"
+}
+
+typeof(caneta)
+console.log(caneta)
+
