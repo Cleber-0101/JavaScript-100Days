e9ffbc82b557267c704000bcda40093bdf1c1d22
Cleber-0101 <cleberbatista99@hotmail.com>
2025-08-31 13:00:18 -0300

    Exercitando logica com java script

---
+++ Dia 10/loops.js
@@ -0,0 +1,15 @@
+
+
+// estruturas de repetiçoes
+
+//contando de 1 a 10 
+for(let i = 0; i <= 10; i++){
+    console.log("contando " + i)
+}
+
+let teste = 0
+
+while(teste <= 1000000000){
+    console.log(teste)
+    teste++
+}
---
+++ Estruturas/estrutura.js
@@ -0,0 +1,16 @@
+// 3 formas de escrever variaveis 
+let nome = "Cleber"
+const idade = 25
+var cidade = "São Paulo"
+
+// Nomes reservados
+//number
+//array
+//objetc
+
+//qualquer numero em JS é conhecido como um inteiro 
+//seja ele float ou tipo inteiro 
+
+
+let numero = 10
+console.log(numero + 10)
