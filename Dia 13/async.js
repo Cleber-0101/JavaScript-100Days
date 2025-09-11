7ec0190f79f185fe05836ac1e625ec6bf414262e
Cleber-0101 <cleberbatista99@hotmail.com>
2025-09-11 08:49:33 -0300

    Exercitando java script assync

--- Dia 13/async.js
+++ Dia 13/async.js
@@ -1,8 +1,19 @@
 console.log("Ainda não executou")
 
-
+// este codigo aqui executa depois de do tempo estabelecido 
 setTimeout(function(){
     console.log("Depois de 8 segundos eu executo essa frase aqui !!!")
 }, 8000)
 
 console.log("Ainda não executou 2 ")
+
+// ja este outro ele executa igual um loop e sempre vai fazendo requisições
+console.log("Este codigo não para")
+
+
+// TRAVEI PARA NÃO CRACHAR O SISTEMA 
+// setInterval(function(){
+//     console.log("eu não paro de executador este codigo pois sou um loop infinito ")
+// }, )
+
+console.log("Este codigo aqui tbm não para /")
