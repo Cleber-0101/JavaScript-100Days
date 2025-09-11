001ab7c9a5c48b9e34f5e384d35411694b06b3e8
Cleber-0101 <cleberbatista99@hotmail.com>
2025-09-11 03:35:55 -0300

    Exercitando Java script assincrono

---
+++ Dia 13/async.js
@@ -0,0 +1,8 @@
+console.log("Ainda não executou")
+
+
+setTimeout(function(){
+    console.log("Depois de 8 segundos eu executo essa frase aqui !!!")
+}, 8000)
+
+console.log("Ainda não executou 2 ")
