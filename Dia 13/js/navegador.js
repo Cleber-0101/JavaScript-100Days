2f87db5cb7d6da45f7f9ed57c48eec33eb005bf9
Cleber-0101 <cleberbatista99@hotmail.com>
2025-09-14 12:31:09 -0300

    Manipulando elementos html e css

--- Dia 13/js/navegador.js
+++ Dia 13/js/navegador.js
@@ -17,3 +17,58 @@ console.log(title)
 //selecionando por class
 const produtos = document.getElementsByClassName("produtos")
 console.log(produtos)
+
+
+//selecionando elementos com Java script seletores CSS
+const produtosQuery = document.querySelectorAll(".produtos")
+console.log(produtosQuery)
+
+//selecionando apenas um elemento
+const unicoElemento = document.querySelector("main")
+console.log(unicoElemento)
+
+
+//metodos para alterar HTML
+// 1 insertBefore
+// 2 appendChild
+// 3 replaceChild
+const p = document.createComment("p")
+const header = title.parentElement;
+header.insertBefore(p , title)
+
+
+//padrão é selecionar , criar , modificar 
+const li = document.querySelector("nav ul")
+const criandoLi = document.createElement("li")
+li.appendChild(criandoLi)
+
+
+// usando replace child, trocando 
+const h2 = document.createElement("h2")
+h2.textContent = "Meu novo titulo"
+header.replaceChild(h2 , title)
+
+
+//Trabalhando com atributos
+const pegandoOprimeiroelementos = document.querySelector("a")
+console.log(pegandoOprimeiroelementos)
+pegandoOprimeiroelementos.setAttribute("href" , "https://www.google.com.br")
+
+console.log(pegandoOprimeiroelementos.getAttribute("href"))
+
+
+
+
+//altura e largura dos elementos 
+const footer = document.querySelector("footer") 
+console.log(footer.offsetWidth)
+console.log(footer.offsetHeight)
+
+console.log(footer.clientHeight)
+console.log(footer.clientWidth)
+
+
+//fazendo um loop e alterado a cor de todos os elementos selecionados
+for(const li of listItens){
+    li.style.background = "red"
+}
